2568ae246e3331e45a0c1a4ae7f836690a31236e
Hachiman999 <30505008+Hachiman999@users.noreply.github.com>
2020-05-13 12:04:30 -0500

    a
    df

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
 const express = require('express');
-const app = express();
-const http = require('http').Server(app);
+const path = require('path')
+//const ejs = require('ejs'); 
+const multer = require('multer')
 
-app.use(express.static(__dirname +"/public")); 
+const app = express();//iniciamos el servidor
+
+const http = require('http').Server(app);//aplicamos el protocolo http
+
+//configuracion de uso
+app.use(express.static(__dirname+'public'));
+
+
+//Configuracion de rutas de archivos
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'))
+
+//configurar multer
+const coleccion = multer.diskStorage({
+    filename: (req,file,cb)=>{
+        cb(null, file.originalname)
+    }
+});
+
+//procesamiento de la imagen
+const subida = multer({
+    dest:path.join(__dirname,'/public/')
+}).single('image');
+
+
+//Configuracion de rutas del servidor
 app.get('/', (req, res) =>{
     //se le dice que por defecto cargue index.html
-    res.sendFile(__dirname+"/public/index.html");
+    res.render('index'); 
     });
 
+
+app.post('/subir',subida, (req, res) =>{
+   console.log(req.file);
+    res.send('subido');
+});
+
+
+
+//iniciamos el servidor
 const PORT =80; 
 
+
 http.listen(PORT ,()=>{
-    console.log("iniciado")
+    console.log(`Servidor iniciado en el puerto ${PORT}`)
 });
